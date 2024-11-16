7d83a5f505aebc6d07b3abd7b579e32be21c58a7
AlexZerocoder <vtumane19@gmail.com>
2024-11-16 14:04:13 +0300

    change the target image

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+
 #инициализируем Pygame
-pygame.init
+pygame.init()
 
 #Создадим игровое окно
 SCREEN_WIDTH = 800
@@ -10,31 +12,41 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 #Зададим название игры
-pygame.display.set_caption(«Game Shooting range»)
+pygame.display.set_caption('Game Shooting range')
 
 #Создадим иконку для игры
-icon = pygame.image.load("img/shooting_range.jpg")
+icon = pygame.image.load("img/Shooting_range.jpg")
 pygame.display.set_icon(icon)
 
-#Зададим рандомные координаты для объекта
-target_image = pygame.image.load("img/target.png")
+#Создадим объект для игры
+target_img = pygame.image.load("img/target.png")
 target_width = 80
 target_height = 80
+
+#Зададим рандомные координаты для объекта
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 #Зададим рандомное значение заливки фона
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-import random
-
-#Создадим объект для игры
-
-
 
 #Цикл
 running = True
-while running:
-    pass
 
+while running:
+    #Зададим заливку экрана
+    screen.fill(color)
+
+    #Создадим события игры
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 #Завершение игры
-pygame.quit
+pygame.quit()
